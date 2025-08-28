a2c2f5c0eff943d44c709865820251089081e98f
Dulaj007 <agyasithadulaj@gmail.com>
2025-08-29 01:59:55 +0530

    project testing environment created

---
+++ app/Views/pages/home.php
@@ -0,0 +1,18 @@
+<!-- pages/home.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home - 24/7 Sentinel</title>
+     <link href="<?= base_url('assets/css/output.css') ?>" rel="stylesheet">
+     <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+
+<?= view('partials/navbar') ?>
+
+<h1>Welcome to 24/7 Sentinel</h1>
+<p>Your CCTV monitoring solution.</p>
+
+</body>
+</html>
