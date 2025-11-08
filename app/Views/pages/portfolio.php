ea5129fce1415503ba2b0c865c48efaa6f12f34a
chathuka55 <chathuka500@gmail.com>
2025-11-08 18:37:33 +0530

    Created Portfolio Page (portfolio.php) and added CSS, icons, and image assets Closes #70

---
+++ app/Views/pages/portfolio.php
@@ -0,0 +1,18 @@
+<!-- Header -->
+<?= $this->include('partials/header') ?>
+
+<?= $this->include('section/portfolioSections/portfolioStyles') ?>
+
+
+<!-- Page content -->
+<?= view('partials/navbar') ?>
+<?= $this->include('section/portfolioSections/portfolioHero') ?>
+<?= $this->include('section/portfolioSections/portfolioFilters') ?>
+<?= $this->include('section/portfolioSections/portfolioGrid') ?>
+
+
+<?= $this->include('section/contactQuick') ?>
+<?= $this->include('section/portfolioSections/scriptPortfolio') ?>
+
+
+<?= view('partials/footer') ?>
